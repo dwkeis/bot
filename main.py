f69c651b8a5b5c50eeb04f9e913aa1374d236670
Ma Keng Chon <dwkeis@gmail.com>
2020-11-25 18:29:40 +0800

    docs: update

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
-from fastapi import FastAPI, Request, Response
+from typing import Optional, List
+
+from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
-import uvicorn
-import httpx
-import os
 
 
-# Init App.
+class Item(BaseModel):
+    object: str = ""
+    entry: List = []
+
+
 app = FastAPI()
 
 
-# Endpoints.
-@app.router.get("/api/webhook")
-async def verify(request: Request):
-    """
-    On webook verification VERIFY_TOKEN has to match the token at the
-    configuration and send back "hub.challenge" as success.
-    """
-    if request.query_params.get(
-        "hub.challenge"
-    ):
-        if (
-            not request.query_params.get("hub.verify_token")
-            == os.environ["VERIFY_TOKEN"]
-        ):
-            return Response(content="Verification token mismatch", status_code=403)
-        return Response(content=request.query_params["hub.challenge"])
-
-    return Response(content="Required arguments haven't passed.", status_code=400)
-
-# Debug.
-def main():
-    if "VERIFY_TOKEN" in os.environ:
-        print("your verify token is: ", os.environ["VERIFY_TOKEN"])
-
-    uvicorn.run(app=app)
-
+@app.post("/")
+async def create_item(item: Item):
+    return item
